cac2decd3c43360641ab7151ffd85ff883cdb128
BudkovMax4 <budkovmmax@gmail.com>
2025-06-23 18:56:21 +0300

    Update database.py
    бд

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
 import sqlite3
+import os
+import json
+
+DATA_FILE = 'database'
 
 def create_connection(db_file='database.py'):
     """ подключение к бд SQLite"""
@@ -32,6 +36,7 @@ def create_table(conn):
 conn = create_connection()
 if conn is not None:
     create_table(conn)
+    print("подкл к бд успешно")
     conn.close()
 else:
     print("Невозможно создать подкл к бд")
@@ -80,6 +85,53 @@ def save_message_to_db(user_id, text):
         )
         conn.commit()
     finally:
+        conn.close()
+
+#загружение файлов
+def load_data():
+        conn = sqlite3.connect(DATA_FILE)
+        cursor = conn.cursor()
+        # Загружаем пользователей
+        cursor.execute("SELECT user_id, data FROM users")
+        users = {row[0]: json.loads(row[1]) for row in cursor.fetchall()}
+    
+        # Загружаем пары
+        cursor.execute("SELECT pair_id, data FROM pairs")
+        pairs = {row[0]: json.loads(row[1]) for row in cursor.fetchall()}
+    
+        conn.close()
+    
+        return {
+            "users": users,
+            "pairs": pairs
+        }
+    
+
+#сохранение данных
+def save_data(data):
+        #Сохранение всех данных в базу данных
+        conn = sqlite3.connect(DATA_FILE)
+        cursor = conn.cursor()
+    
+        # Очищаем таблицы перед сохранением
+        cursor.execute("DELETE FROM users")
+        cursor.execute("DELETE FROM pairs")
+        
+        # Сохраняем пользователей
+        for user_id, user_data in data["users"].items():
+            cursor.execute(
+                "INSERT INTO users (user_id, data) VALUES (?, ?)",
+                (user_id, json.dumps(user_data, ensure_ascii=False))
+            )
+        
+        # Сохраняем пары
+        for pair_id, pair_data in data["pairs"].items():
+            cursor.execute(
+                "INSERT INTO pairs (pair_id, data) VALUES (?, ?)",
+                (pair_id, json.dumps(pair_data, ensure_ascii=False))
+            )
+        
+        conn.commit()
         conn.close()
 
 #zfdf
